c82b18c10610d136a030f537fe2ebf21a694d093
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-12-23 05:49:21 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import openai
+
+st.header("Evaluador de ensayos")
+
+# Inicializa el modelo de lenguaje GPT-3
+model_engine = "text-davinci-003"
+
+# Crea una caja de diálogo para que el usuario ingrese su solicitud
+essay = st.text_area("Pegue su ensayo:")
+
+# Genera una respuesta utilizando el modelo de lenguaje GPT-3
+if essay:
+    prompt = (f"Su tarea es evaluar la calidad de los ensayos académicos que se le presentan."
+             f"Evalúelos como lo haría un profesor de Harvard y póngales una nota de cero a cien.\n{essay}\n")
+    completions = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        temperature=0.5,
+        top_p=1,
+        frequency_penalty=1,
+        presence_penalty=1
+    )
+
+    message = completions.choices[0].text
+
+    # Obtiene la respuesta generada y la muestra en otra caja de diálogo
+    response = completions.choices[0].text
+    st.markdown(f"**Respuesta:** {response}")
